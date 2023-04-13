4de87cb1bfa556a4549368b58a7091e8a47ccc81
jpfirmo <127751417+jpfirmo@users.noreply.github.com>
2023-04-13 14:49:33 -0300

    Add files via upload

---
+++ questao_07.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class questao_07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner entrada = new Scanner (System.in);
+		System.out.println("digite uma letra: ");
+		String letra = entrada.next();
+		
+		switch(letra) {
+		case "a":
+			System.out.println("vogal");
+			break;
+		case "e":
+			System.out.println("vogal");
+			break;
+		case "i":
+			System.out.println("vogal");
+			break;
+		case "o":
+			System.out.println("vogal");
+			break;
+		case "u": 
+			System.out.println("vogal");
+			break;
+		default:
+			System.out.println("consoante");
+		}
+		
+		
+		
+		
+		
+		
+	}
+
+}
