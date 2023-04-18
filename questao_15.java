7f7bdfe8eb54248c2b9d24fc8bc9852279a53df0
jpfirmo <127751417+jpfirmo@users.noreply.github.com>
2023-04-18 17:31:42 -0300

    Add files via upload

---
+++ questao_15.java
@@ -0,0 +1,18 @@
+
+public class questao_15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int contador = 1;
+		while(contador < 101) {
+			contador = contador + 10;
+			System.out.println(contador);
+		}
+		
+		
+		
+		
+	}
+
+}
